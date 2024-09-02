6559f5a5843335e5e4e3388391ce2f4abe76639f
Kyunghwan Kim <nzmars@gmail.com>
2024-09-02 19:10:21 +0900

    update

--- scripts/build_swig.sh
+++ scripts/build_swig.sh
@@ -9,11 +9,12 @@ echo BUILD ORESWIG
 cd ${ORE_SWIG_DIR}/OREAnalytics-SWIG/Python
 
 cmake -Wno-dev -B $BUILD -G $GENERATOR                            \
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE                            \
         -DORE=$ORE_ROOT_DIR -DORE_BUILD=$ORE_ROOT_DIR/$BUILD      \
         -DORE_USE_ZLIB=OFF -DBoost_NO_SYSTEM_PATHS=ON             \
-        -DBOOST_LIBRARYDIR=$BOOST_LIBRARYDIR                      \
+        -DBOOST_LIBRARYDIR=$BOOST_LIB                             \
         -DBOOST_ROOT=$BOOST -DBoost_ROOT=$BOOST                   \
-        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_SHARED_LIBS=OFF -DBoost_USE_STATIC_LIBS=ON
         # -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS_RELEASE='-O1 -DNDEBUG'
 
 cmake --build $BUILD --parallel 2 --config $BUILD_TYPE --verbose 
