b3f71b4919246a681c09c9d1a90f85415e931ea2
Kyunghwan Kim <nzmars@nzmars-mac.local>
2026-09-02 20:35:01 +0900

    update

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,37 +5,35 @@
 
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
-export ORE=`pwd -P`                 # required
+export ORE=$(pwd -P) # required
 ORE_ROOT_DIR=$ORE
 ORE_DIR=$ORE_ROOT_DIR
-ORE_SWIG_DIR=`realpath ../ORE-SWIG`
+# ORE_SWIG_DIR=$(realpath ../ORE-SWIG)
+ORE_SWIG_DIR=$(cd ORE-SWIG && pwd)
 
-
-BOOST_ROOT=`realpath ../boost_1_86_0`
+# BOOST_ROOT=$(realpath ../boost_1_86_0)
+BOOST_ROOT=$(cd ../boost_1_86_0 && pwd)
 BOOST=$BOOST_ROOT
-export BOOST_INC=$BOOST             # required
-export BOOST_LIB=$BOOST/stage/lib   # required
+export BOOST_INC=$BOOST           # required
+export BOOST_LIB=$BOOST/stage/lib # required
 # BOOST_LIBRARYDIR=$BOOST_LIB
-
 
 # export LDFLAGS='-Wl,-Bstatic'
 # export CXXFLAGS='-NDEBUG'
 
-
 GENERATOR=Ninja
 BUILD=build
 BUILD_TYPE=Release
-BIN=`pwd -P`/bin
+BIN=$(pwd -P)/bin
 
 # ------------------------------------------------------------------
 # flag_use_vcpkg: 1 (use vcpkg), 0(use boost)
 # ------------------------------------------------------------------
 flag_use_vcpkg=0
 
-cores=$(nproc)
-CPU_N=$((cores/2))
+cores=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
+CPU_N=$((cores / 2))
 CPU_N=10
 
-
 echo ORE_ROOT_DIR=$ORE_ROOT_DIR
-echo BUILD=$BUILD   BIN=$BIN
+echo BUILD=$BUILD BIN=$BIN
